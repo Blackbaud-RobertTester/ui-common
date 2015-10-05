6419163f0fcbf9e1b125cac23fe697a2c5e0a718
Blackbaud-RobertTester <robert.tester@blackbaud.com>
2015-10-05 17:02:02 -0500

    More work on build system

---
+++ gulp/conf.js
@@ -0,0 +1,29 @@
+var gutil = require('gulp-util');
+
+/**
+ *  The main paths of your project handle these with care
+ */
+//TODO: Should not have to hard code the src here.  What we really want is the file to be created in the domain1 directory and then copied.
+exports.paths = {
+    src: 'src/app/ui-common',
+    dist: 'dist',
+    tmp: '.tmp'
+};
+
+exports.module = {
+    exportFileName: 'common.js',
+    mainModuleFileName: 'main.module.js',
+    moduleName: 'uiShell.common'
+}
+
+/**
+ *  Common implementation for an error handler of a Gulp plugin
+ */
+exports.errorHandler = function (title) {
+    'use strict';
+
+    return function (err) {
+        gutil.log(gutil.colors.red('[' + title + ']'), err.toString());
+        this.emit('end');
+    };
+};
